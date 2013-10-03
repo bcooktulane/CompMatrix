417c5ef10955f8a47edce3bcf3b57a79e8926a1a
Ubuntu <ubuntu@ip-172-31-8-224.us-west-2.compute.internal>
2013-10-03 21:32:26 +0000

    added ContactForm class and imports

--- pricecompare/pricecompare/models.py
+++ pricecompare/pricecompare/models.py
@@ -1,5 +1,14 @@
 from decimal import Decimal
 from django.db import models
+from django import newforms as forms
+from django.newforms.widgets import *
+from django.core.mail import send_mail, BadHeaderError
+
+class ContactForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    topic = forms.CharField()
+    message = forms.CharField(widget=Textarea())
 
 
 class State(models.Model):
